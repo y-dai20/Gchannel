4c97cdbc1d25b5c99e853e6b64fc66e629a1bddd
y-dai20 <funkymonster20@gmail.com>
2022-07-17 22:18:13 +0900

    use room model

--- base/views/room_views.py
+++ base/views/room_views.py
@@ -4,12 +4,15 @@ from base.models import Room
 class ShowRoomView(ListView):
     template_name = 'pages/room.html'
     model = Room
-    paginate_by = 10
-
+    
     def get_context_data(self, **kwargs):
         context =  super().get_context_data(**kwargs)
-        room = Room.objects.get(admin_user=self.request.user)
-        context["title"] = room.title
-        context["subtitle"] = room.subtitle
+        context["title"] = "Title"
+        context["subtitle"] = "Subtitle"
+
+        room = Room.objects.filter(admin_user=self.request.user)
+        if room.exists():
+            context["title"] = room[0].title
+            context["subtitle"] = room[0].subtitle
 
         return context
